f4273498af32d8f0bc702db89896233cf70d653b
nick avlo <avlo@yahoo.com>
2025-04-21 15:39:22 -0700

    update dependency

--- src/main/java/com/prosilion/afterimage/util/NostrRelayService.java
+++ src/main/java/com/prosilion/afterimage/util/NostrRelayService.java
@@ -1,6 +1,7 @@
 package com.prosilion.afterimage.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.prosilion.subdivisions.service.NostrRelayClient;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -13,45 +14,43 @@ import nostr.event.impl.GenericEvent;
 import nostr.event.message.EventMessage;
 import nostr.event.message.OkMessage;
 import nostr.event.message.ReqMessage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ssl.SslBundle;
 import org.springframework.boot.ssl.SslBundles;
 
 @Slf4j
 public class NostrRelayService {
-  private final com.prosilion.subdivisions.service.NostrRelayService nostrRelayService;
+  private final NostrRelayClient nostrRelayClient;
 
   public NostrRelayService(@NonNull String relayUri) throws ExecutionException, InterruptedException {
     log.debug("relayUri: \n{}", relayUri);
-    this.nostrRelayService = new com.prosilion.subdivisions.service.NostrRelayService(relayUri);
+    this.nostrRelayClient = new NostrRelayClient(relayUri);
     System.out.println("relayUri: " + relayUri);
   }
 
-  public NostrRelayService(@NonNull String relayUri, @NonNull SslBundles sslBundles
-  ) throws ExecutionException, InterruptedException {
+  public NostrRelayService(@NonNull String relayUri, @NonNull SslBundles sslBundles) throws ExecutionException, InterruptedException {
     log.debug("relayUri: \n{}", relayUri);
     log.debug("sslBundles: \n{}", sslBundles);
     final SslBundle server = sslBundles.getBundle("server");
     log.debug("sslBundles name: \n{}", server);
     log.debug("sslBundles key: \n{}", server.getKey());
     log.debug("sslBundles protocol: \n{}", server.getProtocol());
-    this.nostrRelayService = new com.prosilion.subdivisions.service.NostrRelayService(relayUri);
+    this.nostrRelayClient = new NostrRelayClient(relayUri, sslBundles);
   }
 
   public OkMessage sendEvent(@NonNull String eventJson) throws IOException {
-    return nostrRelayService.sendEvent(eventJson);
+    return nostrRelayClient.sendEvent(eventJson);
   }
 
   public OkMessage sendEvent(@NonNull EventMessage eventMessage) throws IOException {
-    return nostrRelayService.sendEvent(eventMessage);
+    return nostrRelayClient.sendEvent(eventMessage);
   }
 
   public List<GenericEvent> sendRequestReturnEvents(@NonNull ReqMessage reqMessage) throws JsonProcessingException {
-    return nostrRelayService.sendRequestReturnEvents(reqMessage);
+    return nostrRelayClient.sendRequestReturnEvents(reqMessage);
   }
 
   public Map<Command, List<String>> sendRequest(@NonNull String reqJson, @NonNull String clientUuid) {
-    Map<Command, List<Object>> resultsMap = nostrRelayService.sendRequestReturnCommandResultsMap(clientUuid, reqJson);
+    Map<Command, List<Object>> resultsMap = nostrRelayClient.sendRequestReturnCommandResultsMap(clientUuid, reqJson);
     List<Object> objects = resultsMap.get(Command.EVENT);
     List<String> returnedEvents = objects.stream().map(Object::toString).toList();
     log.debug("socket [{}] getEvents():", clientUuid);
