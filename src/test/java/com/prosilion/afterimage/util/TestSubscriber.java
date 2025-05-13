257031e3763e5cac6ba0dae8410433ad890328d2
nick avlo <avlo@yahoo.com>
2025-05-12 17:06:21 -0700

    as per subdivisions TestSubscriber class, hack-ey handling for subscriber whose onNext() is never called

--- src/test/java/com/prosilion/afterimage/util/TestSubscriber.java
+++ src/test/java/com/prosilion/afterimage/util/TestSubscriber.java
@@ -11,9 +11,26 @@ import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 
 public class TestSubscriber<T> extends BaseSubscriber<T> {
+  public enum Mode {
+    WAIT_FOR_COMPLETE_ATOMIC_BOOL__CANONICAL_MODE,
+    DO_NOT_WAIT_FOR_COMPLETE_ATOMIC_BOOL__FLUX_KNOWN_TO_HAVE_NO_RETURNED_ITEMS__NEEDS_FIXING
+  }
+
   private final List<T> items = Collections.synchronizedList(new ArrayList<>());
-  private final AtomicBoolean completed = new AtomicBoolean(false);
+  private final AtomicBoolean completed;
   private Subscription subscription;
+
+  public TestSubscriber() {
+    this(Mode.WAIT_FOR_COMPLETE_ATOMIC_BOOL__CANONICAL_MODE);
+  }
+
+  public TestSubscriber(Mode mode) {
+    super();
+    this.completed = mode.equals(
+        Mode.DO_NOT_WAIT_FOR_COMPLETE_ATOMIC_BOOL__FLUX_KNOWN_TO_HAVE_NO_RETURNED_ITEMS__NEEDS_FIXING) ? 
+        new AtomicBoolean(true) : 
+        new AtomicBoolean(false);
+  }
 
   @Override
   public void hookOnSubscribe(@NonNull Subscription subscription) {
@@ -23,9 +40,9 @@ public class TestSubscriber<T> extends BaseSubscriber<T> {
 
   @Override
   public void hookOnNext(@NonNull T value) {
-    completed.setRelease(false);
+    completed.set(false);
     subscription.request(1);
-    completed.setRelease(true);
+    completed.set(true);
     items.add(value);
   }
 
@@ -40,6 +57,6 @@ public class TestSubscriber<T> extends BaseSubscriber<T> {
 
   @Override
   protected void hookOnComplete() {
-    completed.setRelease(true);
+    completed.set(true);
   }
 }
