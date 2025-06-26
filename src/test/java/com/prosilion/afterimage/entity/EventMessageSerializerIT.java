d2e023c4bb744b47080af296b0dea7337353e571
nick avlo <avlo@yahoo.com>
2025-06-26 14:07:07 -0700

    dependencies up

--- src/test/java/com/prosilion/afterimage/entity/EventMessageSerializerIT.java
+++ src/test/java/com/prosilion/afterimage/entity/EventMessageSerializerIT.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 // TODO: should not require SpringBootTest just to load EventMessage class' EventMessageDeserializer ctor() List<KindTypeIF> kindType.  needs revisit
 @SpringBootTest
 @ActiveProfiles("test")
-public class EventMessageSerializerTest {
+public class EventMessageSerializerIT {
   @Test
   void testEventMessageGenericEventKindTypeEncoder() throws IOException, NostrException {
     EventMessage eventMessage = new EventMessage(new GenericEventKindType("5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e590001", new PublicKey("bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984"), 1111111111111L, Kind.BADGE_AWARD_EVENT, List.of(new AddressTag(Kind.BADGE_DEFINITION_EVENT, new PublicKey("bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984"), new IdentifierTag(AfterimageKindType.UPVOTE.getName()))), "matching kind, author, identity-tag filter test", Signature.fromString("86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546"), List.of(AfterimageKindType.values())));
