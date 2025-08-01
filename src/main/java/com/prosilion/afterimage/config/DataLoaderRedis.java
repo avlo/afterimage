c791ab6d89ad9643e527de0186a8915b08992659
nick avlo <avlo@yahoo.com>
2025-08-01 14:29:45 -0700

    DataLoaderRedisIF

---
+++ src/main/java/com/prosilion/afterimage/config/DataLoaderRedis.java
@@ -0,0 +1,26 @@
+package com.prosilion.afterimage.config;
+
+import com.prosilion.nostr.event.BadgeDefinitionEvent;
+import com.prosilion.superconductor.autoconfigure.redis.config.DataLoaderRedisIF;
+import com.prosilion.superconductor.base.service.event.type.EventPluginIF;
+import com.prosilion.superconductor.lib.redis.dto.GenericDocumentKindDto;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.NonNull;
+
+public class DataLoaderRedis implements DataLoaderRedisIF {
+  private final EventPluginIF eventPlugin;
+  private final BadgeDefinitionEvent reputationBadgeDefinitionEvent;
+
+  public DataLoaderRedis(
+      @NonNull @Qualifier("eventPlugin") EventPluginIF eventPlugin,
+      @NonNull BadgeDefinitionEvent reputationBadgeDefinitionEvent) {
+    this.eventPlugin = eventPlugin;
+    this.reputationBadgeDefinitionEvent = reputationBadgeDefinitionEvent;
+  }
+
+  @Override
+  public void run(String... args) {
+    eventPlugin.processIncomingEvent(
+        new GenericDocumentKindDto(reputationBadgeDefinitionEvent).convertBaseEventToEventIF());
+  }
+}
