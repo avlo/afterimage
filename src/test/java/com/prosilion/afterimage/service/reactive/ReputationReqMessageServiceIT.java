cd5d98f28cbb461911488e5195dfcd46201862e1
nick avlo <avlo@yahoo.com>
2025-08-07 15:29:13 -0700

    ITs up

--- src/test/java/com/prosilion/afterimage/service/reactive/ReputationReqMessageServiceIT.java
+++ src/test/java/com/prosilion/afterimage/service/reactive/ReputationReqMessageServiceIT.java
@@ -2,7 +2,7 @@ package com.prosilion.afterimage.service.reactive;
 
 import com.prosilion.afterimage.enums.AfterimageKindType;
 import com.prosilion.afterimage.event.BadgeAwardUpvoteEvent;
-import com.prosilion.afterimage.relay.AfterimageMeshRelayService;
+import com.prosilion.afterimage.util.AfterimageMeshRelayService;
 import com.prosilion.afterimage.util.Factory;
 import com.prosilion.afterimage.util.TestSubscriber;
 import com.prosilion.nostr.NostrException;
