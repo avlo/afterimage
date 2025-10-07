d3fe2698e3e0d51ed00c2cd4b858cf682f3d6433
nick avlo <avlo@yahoo.com>
2025-10-08 00:03:19 +0800

    successful local dev test containers

--- src/test/java/com/prosilion/afterimage/config/LocalDevTestcontainersConfig.java
+++ src/test/java/com/prosilion/afterimage/config/LocalDevTestcontainersConfig.java
@@ -16,8 +16,24 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class LocalDevTestcontainersConfig {
   @Bean
   @ServiceConnection(name = "redis")
-  public ComposeContainer composeContainer() {
-    return new ComposeContainer(
+  public ComposeContainer composeContainer_1() {
+    ComposeContainer composeContainer = new ComposeContainer(
         new File("src/test/resources/docker-compose-local_ws.yml"));
+    return composeContainer;
+  }
+
+  public static final String SUPERCONDUCTOR_AFTERIMAGE = "superconductor-afterimage";
+  public static final String AFTERIMAGE_APP = "afterimage-app";
+
+  @Bean
+  @ServiceConnection(name = "redis")
+  public ComposeContainer composeContainer_2() {
+    ComposeContainer composeContainer = new ComposeContainer(
+        new File("src/test/resources/afterimage-docker-compose-local-dev/afterimage-docker-compose-dev-test-ws.yml"))
+//        .withExposedService(SUPERCONDUCTOR_AFTERIMAGE, 5555)
+//        .withExposedService(AFTERIMAGE_APP, 5557)
+//        .withRemoveVolumes(true)
+        ;
+    return composeContainer;
   }
 }
