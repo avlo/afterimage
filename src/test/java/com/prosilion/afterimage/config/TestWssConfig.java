edd9e991b644f71e29591fecc3a73c3d7f1f6cc2
nick avlo <avlo@yahoo.com>
2026-03-21 17:25:23 +0800

    tests up, superfluous rmd

--- src/test/java/com/prosilion/afterimage/config/TestWssConfig.java
+++ src/test/java/com/prosilion/afterimage/config/TestWssConfig.java
@@ -1,26 +1,11 @@
 package com.prosilion.afterimage.config;
 
-import com.prosilion.afterimage.util.AfterimageReactiveRelayClient;
-import java.util.concurrent.ExecutionException;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.ssl.SslBundles;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.lang.NonNull;
 
 @Configuration
 @ConditionalOnProperty(
     name = "server.ssl.enabled",
     havingValue = "true")
 public class TestWssConfig {
-
-  @Bean
-  @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  public AfterimageReactiveRelayClient afterimageReactiveRelayClient(
-      @NonNull String afterimageRelayUrl,
-      @NonNull SslBundles sslBundles) throws ExecutionException, InterruptedException {
-    return new AfterimageReactiveRelayClient(afterimageRelayUrl, sslBundles);
-  }
 }
