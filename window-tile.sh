a202bb9b31b334dab26da4a933cb6cc693081240
nick avlo <avlo@yahoo.com>
2026-03-31 19:15:47 +0800

    addnl quicks

--- window-tile.sh
+++ window-tile.sh
@@ -8,7 +8,7 @@ TERMINAL_CD=$WINDOW_TILE_TEMP_DIR/terminal_cd
 TERMINAL_TITLE=$WINDOW_TILE_TEMP_DIR/terminal_title
 TERMINAL_ENV=$WINDOW_TILE_TEMP_DIR/terminal_env
 
-horizontal_terminal_count=3
+horizontal_terminal_count=2
 vertical_terminal_count=2
 zoom_factor=.7
 
@@ -117,22 +117,25 @@ append_terminal_files() {
   fi
 }
 
-create_docker_terminal_names() {
-#  row 1
+sc_redis_6379() {
   append_terminal_files "SC redis 6379" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/superconductor" $TERMINAL_CD
   append_terminal_files "sc_redis_6379" $TERMINAL_ENV
   dock_up_alias="docker compose -f superconductor/redis/docker-compose-local_ws.yml up -d && docker container ls -a | grep superconductor-db-redis-local | cut -c 1-12 | xargs docker logs -f"
   dock_down_alias="docker compose -f superconductor/redis/docker-compose-local_ws.yml stop && docker compose -f superconductor/redis/docker-compose-local_ws.yml down --remove-orphans"
   create_docker_terminal_env "sc_redis_6379" "$dock_up_alias" "$dock_down_alias"
-  
+}
+
+aimg_redis_6381() {
   append_terminal_files "aImg redis 6381" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/afterimage" $TERMINAL_CD
   append_terminal_files "aimg_redis_6381" $TERMINAL_ENV
   dock_up_alias="docker compose -f docker-compose-local_ws_6381.yml up -d && docker container ls -a | grep afterimage-db-redis-local_ws_6381 | cut -c 1-12 | xargs docker logs -f"
   dock_down_alias="docker compose -f docker-compose-local_ws_6381.yml down --remove-orphans"
   create_docker_terminal_env "aimg_redis_6381" "$dock_up_alias" "$dock_down_alias"
-  
+}
+
+aimg_redis_6382() {
   append_terminal_files "aImg redis 6382" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/afterimage" $TERMINAL_CD
   append_terminal_files "aimg_redis_6382" $TERMINAL_ENV
@@ -141,25 +144,38 @@ create_docker_terminal_names() {
   create_docker_terminal_env "aimg_redis_6382" "$dock_up_alias" "$dock_down_alias"
 }
 
-create_app_terminal_names() {
-#  row 2
+sc_app_5555() {
   append_terminal_files "SC 5555" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/superconductor" $TERMINAL_CD
   append_terminal_files "sc_app_5555" $TERMINAL_ENV
   app_up_alias="gradle superconductor-app-redis:bootRunLocalWs -Pserver.port=5555 -Psuperconductor.relay.url=ws://localhost:5555 -Plogging.level.com.prosilion.superconductor=debug -Plogging.level.com.prosilion.subdivisions=debug"
   create_app_terminal_env "sc_app_5555" "$app_up_alias"
+}
 
+aimg_app_5557() {
   append_terminal_files "aImg 5557" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/afterimage" $TERMINAL_CD
   append_terminal_files "aimg_app_5557" $TERMINAL_ENV  
   app_up_alias="gradle bootRunLocalWs -Pserver.port=5557 -Pafterimage.relay.url=ws://localhost:5557 -Pspring.data.redis.port=6381"
   create_app_terminal_env "aimg_app_5557" "$app_up_alias"
+}
 
+aimg_app_5558() {
   append_terminal_files "aImg 5558" $TERMINAL_TITLE
   append_terminal_files "/home/nick/git/afterimage" $TERMINAL_CD
   append_terminal_files "aimg_app_5558" $TERMINAL_ENV
   app_up_alias="gradle bootRunLocalWs -Pserver.port=5558 -Pafterimage.relay.url=ws://localhost:5558 -Pspring.data.redis.port=6382"
   create_app_terminal_env "aimg_app_5558" "$app_up_alias"
+}
+
+create_terminal_names() {
+#  sc_redis_6379
+  aimg_redis_6381
+  aimg_redis_6382
+  
+#  sc_app_5555
+  aimg_app_5557
+  aimg_app_5558
 }
 
 create_terminals() {
@@ -210,7 +226,7 @@ user_prompt_start_terminals() {
   (enter/default) -> start): " -a args_array
     
     [[ ${#args_array[@]} -eq 0 ]] && 
-      { create_docker_terminal_names;
+      { create_terminal_names;
         create_app_terminal_names;
         create_terminals;
 			  get_new_terminal_pids;	
